bfe980d1117dca31e85f3dd7820a1927ed546d3b
Milosevic <bm250243@ncr.com>
2018-03-14 03:27:23 -0400

    updated js

--- js/Binary_to_Decimal.js
+++ js/Binary_to_Decimal.js
@@ -1,14 +1,46 @@
 class App extends React.Component{
+	constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.state={
+      	value:'',
+      }
+  	}
+
+  	setData(result){
+		this.setState({value: result})
+		console.log(result)
+	}
+
+	handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+	handleSubmit(event){
+		const binary = this.state.value
+		console.log('entered value:' + binary);
+		const digit = parseInt(binary, 2);
+		alert("Result is" + " " + digit);
+		event.preventDefault();
+	}
+
 	render(){
 		const jsx=(
 			<div>
-				<h1>Binary to Decimal Converter</h1>
-			</div>
-			<div>
-				<input type="number"/>
-				<input type="submit" value="Change"/>
+				<div>
+					<form onSubmit={this.handleSubmit} id="form">
+						<input type="number" value={this.state.value} onChange={this.handleChange} maxLength="8" id="binNum"/>
+						<input type="submit" value="Change"/>
+					</form>
+				</div>
+				<div>
+					<p>Result is:</p>
+				</div>
 			</div>
 		);
 		return jsx;
 	}
 }
+const root=document.getElementById("root");
+ReactDOM.render(<App />,root);
